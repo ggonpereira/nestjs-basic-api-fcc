3399182bd4771e96a8e016b49ef1192db1589e4d
Gabriel Pereira <ggonpereira@gmail.com>
2022-11-22 19:37:25 -0300

    ✨ feat: create signUp service function

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,43 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+@Injectable({})
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signUp(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+          email: dto.email,
+          passwordHash: hash,
+        },
+      });
+
+      delete user.passwordHash;
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials already taken');
+        }
+      }
+
+      throw error;
+    }
+  }
+
+  signIn() {
+    return {
+      message: 'Hey I am signed in',
+    };
+  }
+}
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class AuthDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsOptional()
+  @IsString()
+  lastName?: string;
+}
