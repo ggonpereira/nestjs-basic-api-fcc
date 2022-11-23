093b14e148824fd058a63ae85d5f753558514dff
Gabriel Pereira <ggonpereira@gmail.com>
2022-11-22 22:07:13 -0300

    👌 improve: create JwtStrategy to check jwt token on headers

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,14 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config/dist';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+}
