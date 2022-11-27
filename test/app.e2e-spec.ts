9c72f79ef4395e707b1e34bdaa66072f06df7db6
Gabriel Pereira <ggonpereira@gmail.com>
2022-11-27 19:13:43 -0300

    🚨 test: add tests for user signup and signin

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,16 @@
 import { Test } from '@nestjs/testing';
-import { AppModule } from '../src/app.module';
 import { INestApplication } from '@nestjs/common/interfaces';
 import { ValidationPipe } from '@nestjs/common';
+import * as pactum from 'pactum';
+import { AppModule } from '../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { SignInDto, SignUpDto } from '../src/auth/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
+  const TEST_PORT = 3334;
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -17,11 +23,146 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+    await app.listen(TEST_PORT);
+
+    prisma = app.get(PrismaService);
+    pactum.request.setBaseUrl(`http://localhost:${TEST_PORT}`);
+
+    await prisma.cleanDb();
   });
 
   afterAll(() => {
     app.close();
   });
 
-  it.todo('should pass');
+  describe('Auth', () => {
+    describe('Sign Up', () => {
+      it('should throw if email is not provided', () => {
+        const dto = {
+          password: '123456',
+          firstName: 'Gabriel',
+        };
+
+        return pactum
+          .spec()
+          .post(`/auth/signup`)
+          .withBody(dto)
+          .expectStatus(400);
+      });
+
+      it('should throw if password is not provided', () => {
+        const dto = {
+          email: 'ggonpereira@gmail.com',
+          firstName: 'Gabriel',
+        };
+
+        return pactum
+          .spec()
+          .post(`/auth/signup`)
+          .withBody(dto)
+          .expectStatus(400);
+      });
+
+      it('should throw if firstName is not provided', () => {
+        const dto = {
+          email: 'ggonpereira@gmail.com',
+          password: '123456',
+        };
+
+        return pactum
+          .spec()
+          .post(`/auth/signup`)
+          .withBody(dto)
+          .expectStatus(400);
+      });
+
+      it('should throw if no body is provided', () =>
+        pactum.spec().post(`/auth/signup`).expectStatus(400));
+
+      it('should signup', () => {
+        const dto: SignUpDto = {
+          email: 'ggonpereira@gmail.com',
+          password: '123456',
+          firstName: 'Gabriel',
+        };
+
+        return pactum
+          .spec()
+          .post(`/auth/signup`)
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('Sign In', () => {
+      it('should throw if email is not provided', () => {
+        const dto = {
+          password: '123456',
+        };
+
+        return pactum
+          .spec()
+          .post(`/auth/signin`)
+          .withBody(dto)
+          .expectStatus(400);
+      });
+
+      it('should throw if password is not provided', () => {
+        const dto = {
+          email: 'ggonpereira@gmail.com',
+        };
+
+        return pactum
+          .spec()
+          .post(`/auth/signin`)
+          .withBody(dto)
+          .expectStatus(400);
+      });
+
+      it('should throw if no body is provided', () =>
+        pactum.spec().post(`/auth/signin`).expectStatus(400));
+
+      it('should signin', () => {
+        const dto: SignInDto = {
+          email: 'ggonpereira@gmail.com',
+          password: '123456',
+        };
+
+        return pactum
+          .spec()
+          .post(`/auth/signin`)
+          .withBody(dto)
+          .expectStatus(200)
+          .stores('userAT', 'access_token');
+      });
+    });
+  });
+
+  describe('User', () => {
+    describe('Get me', () => {
+      it('should get current user', () => {
+        return pactum
+          .spec()
+          .get(`/users/me`)
+          .withHeaders({
+            Authorization: 'Bearer $S{userAT}',
+          })
+          .expectStatus(200);
+      });
+    });
+
+    describe('Edit user', () => {});
+  });
+
+  describe('Bookmarks', () => {
+    describe('Get bookmark', () => {});
+
+    describe('Create bookmark', () => {});
+
+    describe('Get bookmark by id', () => {});
+
+    describe('Edit bookmark', () => {});
+
+    describe('Delete bookmark', () => {});
+  });
 });
